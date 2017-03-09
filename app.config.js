aa7027c8b516f051250b7db85c2a9bfc53aeef96
Vikas <vikas.chauhan.bb@gmail.com>
2017-03-09 16:34:47 +0530

    add work template

--- app.config.js
+++ app.config.js
@@ -5,5 +5,8 @@ angular.module("myApp").config(function($routeProvider){
   .when("/skill",{
     templateUrl:'skill/skill.template.html'
   })
+  .when("/work",{
+  	templateUrl:'work/work.template.html'
+  })
   .otherwise("/");
 });
