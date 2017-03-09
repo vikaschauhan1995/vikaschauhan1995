748beef963c1e6c702ef800aef8416ae6156e7bc
Vikas <vikas.chauhan.bb@gmail.com>
2017-03-09 16:28:47 +0530

    create skill template

--- app.config.js
+++ app.config.js
@@ -1,5 +1,9 @@
 'use strict';
 
-
-
-
+angular.module("myApp").config(function($routeProvider){
+  $routeProvider
+  .when("/skill",{
+    templateUrl:'skill/skill.template.html'
+  })
+  .otherwise("/");
+});
